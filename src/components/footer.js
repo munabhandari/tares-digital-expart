aa0825b6934fddbad084b163d1a2f9cd707b3312
muna bhandari <hotline.muna@gmail.com>
2021-07-01 23:30:43 +0545

    footer js and scss file created

--- src/components/footer.js
+++ src/components/footer.js
@@ -2,7 +2,32 @@ import react from 'react';
 
 const Footer = function() {
     return(
-        <footer>This is footer</footer>
+        <footer>            
+         <div className="footer-up">
+         <div className="container">
+             <div className="row">
+                 <div className="col-md-3">
+                     <h4>Get in Touch</h4>                    	
+                    <p>(800) 123 456 789</p>
+
+                    <p>+1 800 123 4567</p>
+
+                    <p>demo@example.com</p>
+
+                 </div>
+                 <div className="col-md-3"></div>
+                 <div className="col-md-3"></div>
+                 <div className="col-md-3"></div>
+             </div>
+         </div>
+         </div>
+
+         <div className="footer-buttom">
+         <div className="container">
+
+           </div>
+             </div>   
+        </footer>
         
     )
 }
