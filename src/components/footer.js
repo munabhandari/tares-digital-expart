d622e0f251a6761cd88fcc3ea2912f9d5f46a338
muna bhandari <hotline.muna@gmail.com>
2021-07-02 16:51:36 +0545

    footer-top section almost completed

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,30 +1,69 @@
-import react from 'react';
+import footerImage1 from '../images/instragram1.png';
+import footerImage2 from '../images/instragram-2.png';
+import footerImage3 from '../images/instragram-4.png';
+import footerImage4 from '../images/instragram-4.png';
+import footerImage5 from '../images/instragram-5.png';
+import footerImage6 from '../images/instragram-6.png';
 
 const Footer = function() {
     return(
         <footer>            
-         <div className="footer-up">
-         <div className="container">
-             <div className="row">
-                 <div className="col-md-3">
-                     <h4>Get in Touch</h4>                    	
-                    <p>(800) 123 456 789</p>
-
-                    <p>+1 800 123 4567</p>
-
-                    <p>demo@example.com</p>
-
+         <div className="footer-top">
+         <div className="container pt-3">
+             <div className="row py-5">
+                 <div className="col-md-3 get-in-touch">
+                     <h4 className="pb-3">Get in Touch</h4>                    	
+                     <p><a href="index.js"><i class="fa fa-map-marker"></i></a>(800) 123 456 789</p>
+                    <p><a href="index.js"><i class="fa fa-phone"></i></a>+1 800 123 4567</p>
+                    <p><a href="index.js"><i class="fa fa-envelope"></i></a>demo@example.com</p>
                  </div>
-                 <div className="col-md-3"></div>
-                 <div className="col-md-3"></div>
-                 <div className="col-md-3"></div>
+
+                 <div className="col-md-3">
+                 <h4 className="pb-3">About Us</h4>
+                 <ul className="about-us">
+                     <li><a href="home">Blog & Articles</a></li>
+                     <li><a href="home">Vintage Stores</a></li>
+                     <li><a href="home">Trekking</a></li>
+                     <li><a href="home">Sightseeing</a></li>
+                     <li><a href="home">Tours</a></li>
+                   </ul>
+                 </div>
+
+                 <div className="col-md-3 Insta-follow">
+                 <h5 className="pb-3">Instagrame Follow</h5>
+                 <img src={footerImage1} alt=""/>
+                 <img src={footerImage2} alt="" />
+                 <img src={footerImage3} alt="" />
+                 <img src={footerImage4} alt="" />
+                 <img src={footerImage5} alt="" />
+                 <img src={footerImage6} alt="" />
+                 </div>
+
+                 <div className="col-md-3 news-letter">
+                 <h4 className="pb-3">Newsletter</h4>
+                  <p>Fill their seed open meat.<br>
+                       Sea you great Saw image stl</p>
+                    <input type="email" placeholder="Email Address"></input><br>
+                    <button>SUBSCRIBE</button>
+                 </div>
              </div>
          </div>
          </div>
 
          <div className="footer-buttom">
          <div className="container">
+              <div className="left-footer">
+              Copyright 2021 Al Right Reserved   
+              </div>
 
+              <div className="right-footer">
+              <ul>
+                  <li><a href="">About us</a></li>
+                  <li><a href="">Service</a></li>
+                  <li><a href="">Privacy Policy</a></li>
+                  <li><a href="">Contact Us</a></li>
+                  </ul>       
+              </div>
            </div>
              </div>   
         </footer>
