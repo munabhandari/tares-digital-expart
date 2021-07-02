ee7fcb0194f2d12ecdd101cae5afa1cf8f6d3491
muna bhandari <hotline.muna@gmail.com>
2021-07-02 17:44:31 +0545

    footer section completed

--- src/components/footer.js
+++ src/components/footer.js
@@ -5,69 +5,72 @@ import footerImage4 from '../images/instragram-4.png';
 import footerImage5 from '../images/instragram-5.png';
 import footerImage6 from '../images/instragram-6.png';
 
-const Footer = function() {
-    return(
-        <footer>            
-         <div className="footer-top">
-         <div className="container pt-3">
-             <div className="row py-5">
-                 <div className="col-md-3 get-in-touch">
-                     <h4 className="pb-3">Get in Touch</h4>                    	
-                     <p><a href="index.js"><i class="fa fa-map-marker"></i></a>(800) 123 456 789</p>
-                    <p><a href="index.js"><i class="fa fa-phone"></i></a>+1 800 123 4567</p>
-                    <p><a href="index.js"><i class="fa fa-envelope"></i></a>demo@example.com</p>
-                 </div>
+const Footer = function () {
+    return (
+        <footer className="py-3">
+            <div className="footer-top">
+                <div className="container">
+                    <div className="row pt-5">
+                        <div className="col-md-3 get-in-touch">
+                            <h4 className="pb-3">Get in Touch</h4>
+                            <p><a href="index.js"><i class="fa fa-map-marker"></i></a>(800) 123 456 789</p>
+                            <p><a href="index.js"><i class="fa fa-phone"></i></a>+1 800 123 4567</p>
+                            <p><a href="index.js"><i class="fa fa-envelope"></i></a>demo@example.com</p>
+                        </div>
 
-                 <div className="col-md-3">
-                 <h4 className="pb-3">About Us</h4>
-                 <ul className="about-us">
-                     <li><a href="home">Blog & Articles</a></li>
-                     <li><a href="home">Vintage Stores</a></li>
-                     <li><a href="home">Trekking</a></li>
-                     <li><a href="home">Sightseeing</a></li>
-                     <li><a href="home">Tours</a></li>
-                   </ul>
-                 </div>
+                        <div className="col-md-3">
+                            <h4 className="pb-3">About Us</h4>
+                            <ul className="about-us">
+                                <li><a href="home">Blog & Articles</a></li>
+                                <li><a href="home">Vintage Stores</a></li>
+                                <li><a href="home">Trekking</a></li>
+                                <li><a href="home">Sightseeing</a></li>
+                                <li><a href="home">Tours</a></li>
+                            </ul>
+                        </div>
 
-                 <div className="col-md-3 Insta-follow">
-                 <h5 className="pb-3">Instagrame Follow</h5>
-                 <img src={footerImage1} alt=""/>
-                 <img src={footerImage2} alt="" />
-                 <img src={footerImage3} alt="" />
-                 <img src={footerImage4} alt="" />
-                 <img src={footerImage5} alt="" />
-                 <img src={footerImage6} alt="" />
-                 </div>
+                        <div className="col-md-3 insta-follow">
+                            <h5 className="pb-3">Instagram Follow</h5>
+                            <img src={footerImage1} alt="" />
+                            <img src={footerImage2} alt="" />
+                            <img src={footerImage3} alt="" />
+                            <img src={footerImage4} alt="" />
+                            <img src={footerImage5} alt="" />
+                            <img src={footerImage6} alt="" />
+                        </div>
 
-                 <div className="col-md-3 news-letter">
-                 <h4 className="pb-3">Newsletter</h4>
-                  <p>Fill their seed open meat.<br>
-                       Sea you great Saw image stl</p>
-                    <input type="email" placeholder="Email Address"></input><br>
-                    <button>SUBSCRIBE</button>
-                 </div>
-             </div>
-         </div>
-         </div>
+                        <div className="col-md-3 newsletter">
+                            <h4 className="pb-3">Newsletter</h4>
+                            <p>Fill their seed open meat.<br />
+                                Sea you great Saw image stl</p>
+                            <input type="email" placeholder="Email Address"></input><br />
+                            <button>SUBSCRIBE</button>
+                        </div>                  
+                        <hr/>
+                    </div>
+                </div>
+            </div>
 
-         <div className="footer-buttom">
-         <div className="container">
-              <div className="left-footer">
-              Copyright 2021 Al Right Reserved   
-              </div>
+            <div className="footer-bottom">
+                <div className="container">
+                    <div className="row">
+                        <div className="col-md-8 left-footer">
+                            Copyright 2021 Al Right Reserved
+                        </div>
 
-              <div className="right-footer">
-              <ul>
-                  <li><a href="">About us</a></li>
-                  <li><a href="">Service</a></li>
-                  <li><a href="">Privacy Policy</a></li>
-                  <li><a href="">Contact Us</a></li>
-                  </ul>       
-              </div>
-           </div>
-             </div>   
+                        <div className="col-md-4 right-footer">
+                            <ul>
+                                <li><a href="">About us</a></li>
+                                <li><a href="">Service</a></li>
+                                <li><a href="">Privacy Policy</a></li>
+                                <li><a href="">Contact Us</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </footer>
-        
+
     )
 }
 
